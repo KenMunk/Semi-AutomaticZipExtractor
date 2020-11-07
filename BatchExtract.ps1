ab2ad887977fd68193d5dc05cca50e3b068256d4
Kenneth Munk <kmunk2@student.sierracollege.edu>
2020-11-07 14:12:16 -0800

    One-Shot Developed
    This content was quickly patched together using as little time as necessary.  This powershell tool was mainly a means to an end rather than a exposition of skill.

---
+++ BatchExtract.ps1
@@ -0,0 +1,28 @@
+#Written by Kenneth Sherwood Munk
+#This is a free to use tool users may modify and republish as they wish.
+
+
+Write-Output "Welcome to the batch extractor\n\n";
+Write-Output -NoNewLine "Would you like to extract all adjacent files? "
+$willConfirm = Read-Host "( [N] / Y )"
+
+
+Switch($willConfirm){
+    Y {Write-Host "The tool will display a confirmation prompt for each file"}
+    Default {Write-Host "The tool will NOT display a confirmation prompt for each file"}
+}
+
+$zips = Get-ChildItem -Filter *.zip -Name;
+
+foreach($file in $zips){
+    
+    Write-Output $zips;
+
+    Switch($willConfirm){
+        Y {Expand-Archive $file -Force -Confirm}
+        Default {Expand-Archive $file -Force}
+    }
+}
+
+Write-Host -NoNewline 'Press any key to continue...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
