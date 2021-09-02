78173ca47ddda4d2ea28f04f9d34ace9144b025d
Kenneth Munk <kmunk2@student.sierracollege.edu>
2021-09-02 13:20:13 -0700

    InitialCommit

--- BatchExtract.ps1
+++ BatchExtract.ps1
@@ -1,28 +1,81 @@
 #Written by Kenneth Sherwood Munk
 #This is a free to use tool users may modify and republish as they wish.
 
-
-Write-Output "Welcome to the batch extractor\n\n";
-Write-Output -NoNewLine "Would you like to extract all adjacent files with prompting? "
-$willConfirm = Read-Host "( [N] / Y )"
-
-
-Switch($willConfirm){
-    Y {Write-Host "The tool will display a confirmation prompt for each file"}
-    Default {Write-Host "The tool will NOT display a confirmation prompt for each file"}
-}
-
-$zips = Get-ChildItem -Filter *.zip -Name;
-
-foreach($file in $zips){
+function unpackAll{
     
-    Write-Output $zips;
+    $potentialPackages = Get-ChildItem -File -Name
 
-    Switch($willConfirm){
-        Y {Expand-Archive $file -Force -Confirm}
-        Default {Expand-Archive $file -Force}
+    foreach($package in $potentialPackages){
+        
+        unpack -file $package.ToString()
+
     }
+
+    $foundDirectories = Get-ChildItem -Directory -Name
+
+    foreach($directory in $foundDirectories){
+        
+        Write-Host "scanning directory $directory"
+        cd $directory
+        unpackAll
+        cd ..
+
+    }
+
+
 }
 
-Write-Host -NoNewline 'Press any key to continue...';
-$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+function unpack{
+    
+    param ( [string] $file = "")
+
+    $fileTokens = $file.Split(".")
+
+    $fileType = $fileTokens[$fileTokens.Length - 1]
+
+    Write-Host $fileType
+
+    Switch($fileType){
+        "zip"
+        {
+            Write-Host "zip file detected"
+        }
+        "7z"
+        {
+            Write-Host "7z file detected"
+        }
+        "rar"
+        {
+            Write-Host "rar, a file detected"
+        }
+        "tar"
+        {
+            Write-Host "tar file detected"
+        }
+        Default
+        {
+            Write-Host "$fileType is not supported, file issue against github repository if you need support added for this"
+        }
+    }
+
+
+}
+
+$scriptLocationRaw = $MyInvocation.MyCommand.Definition.Split("\")
+$scriptLocation = $scriptLocationRaw[0]
+[int] $scriptPathLength = $scriptLocationRaw.Length - 1
+
+for([int]$branch = 1; $branch -lt $scriptPathLength; $branch++){
+    
+    $scriptLocation += "\"+$scriptLocationRaw[$branch]
+
+}
+
+Write-Host "Script directory: $scriptLocation"
+Write-Host $PWD
+
+cd $PWD 
+
+
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+unpackAll
